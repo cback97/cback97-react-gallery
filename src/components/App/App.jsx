9b22919d03b9f1415e13aec65b2c19f36cc02dd1
Christian Back <cdirt27@gmail.com>
2021-05-02 02:33:28 -0500

    created axios.get function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,23 @@ import GalleryItem from '../GalleryItem/GalleryItem'
 
 function App() {
 
-  
+let [galleryData, SetGalleryData] = useState([]);
+
+  const getImg = () => {
+
+    axios.get('/gallery')
+    .then(response => {
+      console.log(response.data)
+    })
+
+  }
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
       </div>
     );
 }
