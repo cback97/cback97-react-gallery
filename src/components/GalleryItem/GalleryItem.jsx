9877f1f4325e95c6fd232575c14a32c060bdf31a
Christian Back <cdirt27@gmail.com>
2021-05-03 01:26:42 -0500

    set up state and function for adjusting to and from img/description to be rendered

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,23 @@
 import useState from 'react'
 import axios from 'axios'
 
+// component to src images, create like add button per image, have a toggle for photo description
 function GalleryItem ({image},{getImg}) {
     
-    return(
+    //set up state and function to swap to and from photo/description upon toggle
+    const [imgtoggle, setImgToggle] = useState(true)
+
+    function setImgToggle () {
+        if (imgtoggle) {
+            setImgToggle(false)
+        } else {
+            setImgToggle(true)
+        }
+    }
+
+
+
+    return (
         
     )
 }
