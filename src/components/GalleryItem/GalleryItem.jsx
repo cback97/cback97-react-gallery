f61dad6948daf5fc53c6a3e12effa27f0c2e195d
Christian Back <cdirt27@gmail.com>
2021-05-03 01:52:00 -0500

    Set up functions to go with soon to be established like button, and toggle img option. PUT to target specific id within array

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,7 @@
 import useState from 'react'
 import axios from 'axios'
 import './GalleryItem.css';
+import { response } from 'express';
 
 // component to src images, create like add button per image, have a toggle for photo description
 function GalleryItem ({image},{getImg}) {
@@ -17,10 +18,23 @@ function GalleryItem ({image},{getImg}) {
     }
 
 // set up function to handle likes on click event 
+function likes(event) {
+let id = event.currentTarget.dataset.id;
+console.log('like button confirmation' id)
+
+axios.put(`/gallery/like/${id}`)
+.then((response) => {
+    console.log('testing', response)
+    getImg();
+})
+.catch(error => {
+    console.log('PUT error', error)
+})
+}
 
     return (
         <div>
-            {state ?}
+            {imgtoggle ? }
         </div>
     )
 }
